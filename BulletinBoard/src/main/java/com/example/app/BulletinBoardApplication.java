79be0f87f98ed4f01f2c01b431861f2630f4b373
Mary Shchur <maryshchur1@gmail.com>
2020-09-30 21:08:36 +0300

    Change configuration properties

--- BulletinBoard/src/main/java/com/example/app/BulletinBoardApplication.java
+++ BulletinBoard/src/main/java/com/example/app/BulletinBoardApplication.java
@@ -1,12 +1,11 @@
 package com.example.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BulletinBoardApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BulletinBoardApplication.class,args);
+        SpringApplication.run(BulletinBoardApplication.class, args);
     }
 }
--- BulletinBoard/src/main/java/com/example/app/config/DatabaseConfig.java
+++ BulletinBoard/src/main/java/com/example/app/config/DatabaseConfig.java
@@ -1,32 +1,33 @@
 package com.example.app.config;
 
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@ConfigurationProperties(prefix = "datasource")
+@Setter
 public class DatabaseConfig {
-    @Value("${spring.datasource.url}")
-    private String postgresUrl;
-    @Value("${spring.datasource.driver-class-name}")
-    private String postgresDriver;
-    @Value("${spring.datasource.username}")
-    private String postgresUser;
-    @Value("${spring.datasource.password}")
-    private String postgresPassword;
+    private String url;
+    private String driver;
+    private String username;
+    private String password;
 
     @Primary
     @Bean
     public DataSource customDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(postgresDriver);
-        dataSource.setUrl(postgresUrl);
-        dataSource.setUsername(postgresUser);
-        dataSource.setPassword(postgresPassword);
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 }
